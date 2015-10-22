2733169078c03d2b42d1b6ff99134305f3f06203
isivroes <kaozdeoz@hotmail.com>
2015-10-21 23:54:33 -0500

    cambios

--- app/src/main/java/unam/isivroes/com/pruebaandroidstudio/ActivityOmar.java
+++ app/src/main/java/unam/isivroes/com/pruebaandroidstudio/ActivityOmar.java
@@ -4,13 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View.view;
+
+import android.view.View;
 import android.widget.TextView;
 import java.util.Random;
 
 public class ActivityOmar extends AppCompatActivity {
     private TextView resultado;
-    private String [] saludos = {"Hola", "Adiós", "Buenos días", "Buenas tardes", "Buenas noches", "Dulces sueños", "Descansa bien", "Hasta pronto", "Cuídate"}
+    private String [] saludos = {"Hola", "Adios", "Buenos dias", "Buenas tardes", "Buenas noches", "Dulces sueÃ±os", "Descansa bien", "Hasta pronto", "Cuidate"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
